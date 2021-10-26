8eaf4936218e967228fed67dd08fef4c53cde50d
NimaAB <nimabewrani98@gmail.com>
2021-10-26 23:48:52 +0200

    begynte litt på admin siden

--- Web-app-oppgave-2/ClientApp/src/app/app.module.ts
+++ Web-app-oppgave-2/ClientApp/src/app/app.module.ts
@@ -5,10 +5,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import {Tab_barComponent} from "./components/tab_bar/tab_bar.component";
+import {Tab_itemComponent} from "./components/tab-item/tab_item.component";
 
 @NgModule({
   declarations: [
     AppComponent,
+    Tab_barComponent,
+    Tab_itemComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
---
+++ Web-app-oppgave-2/ClientApp/src/app/components/tab-item/tab_item.component.ts
@@ -0,0 +1,13 @@
+﻿import {Component, Input} from "@angular/core";
+
+@Component({
+  selector:'app-tab-item',
+  templateUrl:'tab_item.component.html',
+})
+export class Tab_itemComponent{
+  @Input() tab:string;
+
+  constructor() {
+  }
+
+}
