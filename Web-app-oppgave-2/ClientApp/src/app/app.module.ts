189fc47c851490ce20cb6dcd71d8b8e836ee0bca
NimaAB <nimabewrani98@gmail.com>
2021-10-27 22:44:46 +0200

    mat-tab

--- Web-app-oppgave-2/ClientApp/src/app/app.module.ts
+++ Web-app-oppgave-2/ClientApp/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
+//import {  } from '@angular/common'
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTabsModule } from '@angular/material/tabs';
+import { RuteCardComponent } from './components/rute-card/rute-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RuteCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Web-app-oppgave-2/ClientApp/src/models/rute.ts
@@ -0,0 +1,12 @@
+﻿export class Rute{
+  tur:string;
+  bilde:any;
+  pris:number;
+
+  constructor(tur:string, bilde:any, pris:number) {
+    this.tur = tur;
+    this.bilde = bilde;
+    this.pris = pris;
+  }
+
+}
