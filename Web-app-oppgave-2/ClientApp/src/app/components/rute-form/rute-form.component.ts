d6879f2195a577080414df86b42416da09c68f98
Glaysa <glaysa.df@gmail.com>
2021-11-01 21:11:01 +0100

    oppdatert rute form komponent

--- Web-app-oppgave-2/ClientApp/src/app/components/rute-form/rute-form.component.ts
+++ Web-app-oppgave-2/ClientApp/src/app/components/rute-form/rute-form.component.ts
@@ -1,40 +1,53 @@
-import { Component, OnInit } from '@angular/core';
-import { Rute } from 'src/models/rute';
-import { Location } from '@angular/common';
-import {FormGroup, Validators, FormControl } from '@angular/forms';
+import { Component } from '@angular/core';
+import {FormGroup, Validators, FormControl, AbstractControl} from '@angular/forms';
 
 @Component({
   selector: 'app-rute-form',
   templateUrl: './rute-form.component.html',
   styleUrls: ['./rute-form.component.css']
 })
-export class RuteFormComponent implements OnInit {
+export class RuteFormComponent {
+  isSubmitted: boolean = false;
+
   form = new FormGroup({
     fra: new FormControl(
-      null,
-      Validators.compose([Validators.required,Validators.pattern("[A-ZÆØÅ][a-zæøå]{2,25}")])
+      null, [
+        Validators.required,
+        Validators.pattern("[A-ZÆØÅ][a-zæøå]{2,25}")]
     ),
     til: new FormControl(
-      null,
-      Validators.compose([Validators.required,Validators.pattern("[A-ZÆØÅ][a-zæøå]{2,25}")])
+      null, [
+        Validators.required,
+        Validators.pattern("[A-ZÆØÅ][a-zæøå]{2,25}")]
     ),
     pris: new FormControl(
-      null,
-      Validators.required
+      null, Validators.required
+    ),
+    bilde: new FormControl(
+      null
     )
   });
 
-  constructor(private location:Location) {
+  get fra(){
+    return this.form.controls.fra;
   }
 
-  ngOnInit(): void {
+  get til(){
+    return this.form.controls.til;
   }
 
-  gaaTilbake() {
-    this.location.back();
+  get pris(){
+    return this.form.controls.pris;
+  }
+
+  get bilde(){
+    return this.form.controls.bilde;
   }
 
   onSubmit() {
+    this.isSubmitted = true;
+    this.form.reset();
+
     /*let rute:string = `rute = {
     \tfra: ${ this.form.value.fra },
     \n\ttil: ${this.form.value.til},
