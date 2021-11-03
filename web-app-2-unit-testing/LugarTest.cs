4d2e69a27b8e3ab12ef9b91ac1ba46ae9e7148c9
Glaysa <glaysa.df@gmail.com>
2021-11-03 17:46:02 +0100

    implementert lugar og måltid testing

---
+++ web-app-2-unit-testing/LugarTest.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using Web_app_oppgave_2.Controllers;
+using Web_app_oppgave_2.DAL.LugarServices;
+using Web_app_oppgave_2.Models;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace web_app_2_unit_testing
+{
+    public class LugarTest
+    {
+        [Test]
+        public async Task HentEnLugar()
+        {
+            var mock = new Mock<ILugarRepository>();
+            mock.Setup(l => l.HentEn(1)).ReturnsAsync(new Lugar());
+            var controller = new LugarController(mock.Object);
+
+            var resultat = await controller.HentLugar(1);
+            var okResult = resultat as ObjectResult;
+            
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual(200, okResult.StatusCode);
+        }
+        
+        [Test]
+        public async Task HentAlleLugarer()
+        {
+            var mock = new Mock<ILugarRepository>();
+            mock.Setup(l => l.HentAlle()).ReturnsAsync(new List<Lugar>());
+            var controller = new LugarController(mock.Object);
+
+            var resultat = await controller.HentAlleLugarer();
+            var okResult = resultat as ObjectResult;
+            
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual(200, okResult.StatusCode);
+        }
+
+        [Test]
+        public async Task OppdaterLugar()
+        {
+            var innLugar = new Lugar
+            {
+                Type = "Rom",
+                Navn = "Standard",
+                Kapasitet = 3,
+                Beskrivelse = "Beskrivelse",
+                MaxReservasjon = 10,
+                Pris = 1099
+            };
+            
+            var mock = new Mock<ILugarRepository>();
+            mock.Setup(l => l.Oppdater(1, innLugar)).ReturnsAsync(true);
+            var controller = new LugarController(mock.Object);
+            
+            var resultat = await controller.OppdaterLugar(1, innLugar);
+            var okResult = resultat as ObjectResult;
+
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual(200, okResult.StatusCode);
+        }
+
+        [Test]
+        public async Task SlettLugar()
+        {
+            var mock = new Mock<ILugarRepository>();
+            mock.Setup(l => l.Slett(1)).ReturnsAsync(true);
+            var controller = new LugarController(mock.Object);
+
+            var resultat = await controller.SlettLugar(1);
+            var okResult = resultat as ObjectResult;
+            
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual(200, okResult.StatusCode);
+        }
+        
+        [Test]
+        public async Task LagreRute()
+        {
+            var innLugar = new Lugar
+            {
+                Type = "Rom",
+                Navn = "Standard",
+                Kapasitet = 3,
+                Beskrivelse = "Beskrivelse",
+                MaxReservasjon = 10,
+                Pris = 1099
+            };
+
+            var mock = new Mock<ILugarRepository>();
+            mock.Setup(l => l.Lagre(innLugar)).ReturnsAsync(true);
+            var controller = new LugarController(mock.Object);
+
+            var resultat = await controller.LagreLugar(innLugar);
+            var okResult = resultat as ObjectResult;
+            
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual(200, okResult.StatusCode);
+        }
+        
+    }
+}
---
+++ web-app-2-unit-testing/MaaltidTest.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using Web_app_oppgave_2.Controllers;
+using Web_app_oppgave_2.DAL.MaaltidServices;
+using Web_app_oppgave_2.Models;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace web_app_2_unit_testing
+{
+    public class MaaltidTest
+    {
+        [Test]
+        public async Task HentEnMaaltid()
+        {
+            var mock = new Mock<IMaaltidRepository>();
+            mock.Setup(m => m.HentEn(1)).ReturnsAsync(new Maaltid());
+            var controller = new MaaltidController(mock.Object);
+
+            var resultat = await controller.HentEnMaaltid(1);
+            var okResult = resultat as ObjectResult;
+            
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual(200, okResult.StatusCode);
+        }
+        
+        [Test]
+        public async Task HentAlleMaaltiderr()
+        {
+            var mock = new Mock<IMaaltidRepository>();
+            mock.Setup(m => m.HentAlle()).ReturnsAsync(new List<Maaltid>());
+            var controller = new MaaltidController(mock.Object);
+
+            var resultat = await controller.HentAlleMaaltider();
+            var okResult = resultat as ObjectResult;
+            
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual(200, okResult.StatusCode);
+        }
+
+        [Test]
+        public async Task OppdaterMaaltid()
+        {
+            var innMaaltid = new Maaltid
+            {
+                Navn = "Frokost",
+                Beskrivelse = "Beskrivelse",
+                Pris = 299,
+            };
+            
+            var mock = new Mock<IMaaltidRepository>();
+            mock.Setup(m => m.Oppdater(1, innMaaltid)).ReturnsAsync(true);
+            var controller = new MaaltidController(mock.Object);
+            
+            var resultat = await controller.OppdaterMaaltid(1, innMaaltid);
+            var okResult = resultat as ObjectResult;
+
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual(200, okResult.StatusCode);
+        }
+
+        [Test]
+        public async Task SlettMaaltid()
+        {
+            var mock = new Mock<IMaaltidRepository>();
+            mock.Setup(m => m.Slett(1)).ReturnsAsync(true);
+            var controller = new MaaltidController(mock.Object);
+
+            var resultat = await controller.SlettMaaltid(1);
+            var okResult = resultat as ObjectResult;
+            
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual(200, okResult.StatusCode);
+        }
+        
+        [Test]
+        public async Task LagreMaaltid()
+        {
+            var innMaaltid = new Maaltid
+            {
+                Navn = "Frokost",
+                Beskrivelse = "Beskrivelse",
+                Pris = 299,
+            };
+
+            var mock = new Mock<IMaaltidRepository>();
+            mock.Setup(m => m.Lagre(innMaaltid)).ReturnsAsync(true);
+            var controller = new MaaltidController(mock.Object);
+
+            var resultat = await controller.LagreMaaltid(innMaaltid);
+            var okResult = resultat as ObjectResult;
+            
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual(200, okResult.StatusCode);
+        }
+    }
+}
