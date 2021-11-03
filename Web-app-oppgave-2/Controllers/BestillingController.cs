dd4a004c26df5457866dea900e7e4761f59f5860
Glaysa <glaysa.df@gmail.com>
2021-11-03 21:37:23 +0100

    implementert bestilling testing

--- Web-app-oppgave-2/Controllers/BestillingController.cs
+++ Web-app-oppgave-2/Controllers/BestillingController.cs
@@ -8,17 +8,19 @@ namespace Web_app_oppgave_2.Controllers
     [Route("[controller]/[action]")]
     public class BestillingController : ControllerBase
     {
-        private readonly IBestillingRepository _db;
+        private readonly IBestillingRepository _repo;
 
-        public BestillingController(IBestillingRepository db)
+        public BestillingController(IBestillingRepository repo)
         {
-            _db = db;
+            _repo = repo;
         }
 
-        public async Task<bool> Lagre(Bestilling innBestilling)
+        public async Task<IActionResult> Lagre(Bestilling innBestilling)
         {
-            bool saved = await _db.Lagre(innBestilling);
-            return saved;
+            var value = await _repo.Lagre(innBestilling);
+            return !value
+                ? BadRequest(new {error = "Noe gikk galt. Bestillingen ble ikke lagret."})
+                : StatusCode(200, new {message = "Ny bestilling er lagret."});
         }
     }
 }
