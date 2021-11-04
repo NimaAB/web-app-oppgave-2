21a42e75731be0ab2ce399ec036ab90f118fbfdb
NimaAB <nimabewrani98@gmail.com>
2021-11-04 12:31:12 +0100

    skrevet imgProcessing.ts men ikke testet enda

---
+++ Web-app-oppgave-2/ClientApp/src/models/imgProcessing.ts
@@ -0,0 +1,28 @@
+﻿export class ImageProcessing{
+  constructor(private imgBytes:string,
+              private imageURL:string) {}
+
+  private set setImgBytes(bytes: string){
+    this.imgBytes = bytes;
+  }
+
+
+  public set setImageURL(url:string){
+    this.imageURL = url;
+  }
+
+  imageToByte(input:HTMLInputElement):string{
+    if(input.files && input.files[0]){
+      let reader = new FileReader();
+      reader.onload = (e)=>{
+        if(e!==null)
+          { // @ts-ignore usikker!!
+            this.setImgBytes(e.target.result);
+          }
+        }
+        //reader.readAsDataURL(input.files[0]);
+      }
+    return this.imgBytes;
+  }
+
+}
